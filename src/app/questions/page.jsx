75342dd964ec81b1f1305f14225c574d296e7ccc
ben rosenbaum <ben@lupa.co.il>
2023-06-30 14:14:04 +0300

    ADD FUNC

--- src/app/questions/page.jsx
+++ src/app/questions/page.jsx
@@ -2,24 +2,25 @@
 import React, { useState, useEffect } from 'react';
 
 const QuestionComponent = ({ questionData, onNext }) => {
-    const [selectedAnswer, setSelectedAnswer] = useState(null);
-    const { subject, question, options, correctAnswer } = questionData;
-  
-    const handleAnswerSelection = (answer) => {
-      setSelectedAnswer(answer);
-    };
-  
-    const checkAnswer = () => {
-      if (selectedAnswer === correctAnswer) {
-        return 'Correct!';
-      }
-      return 'Incorrect!';
-    };
-  
-    return (
-      <div>
-        <h2>{subject}</h2>
-        <p>{question}</p>
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const { subject, question, options, correctAnswer } = questionData;
+
+  const handleAnswerSelection = (answer) => {
+    setSelectedAnswer(answer);
+  };
+
+  const checkAnswer = () => {
+    if (selectedAnswer === correctAnswer) {
+      return 'Correct!';
+    }
+    return 'Incorrect!';
+  };
+
+  return (
+    <div>
+      <h2>{subject}</h2>
+      <p>{question}</p>
+      {options && options.length > 0 ? (
         <ul>
           {options.map((option) => (
             <li key={option}>
@@ -35,49 +36,38 @@ const QuestionComponent = ({ questionData, onNext }) => {
             </li>
           ))}
         </ul>
-        <button onClick={checkAnswer}>Check Answer</button>
-        {selectedAnswer && <p>{checkAnswer()}</p>}
-        <button onClick={onNext}>Next</button>
-      </div>
-    );
-  };
-  
-  export default function QUESTIONS() {
-    const [questionData, setQuestionData] = useState(null);
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  
-    useEffect(() => {
-      const fetchQuestionData = async () => {
-        try {
-          const response = await fetch('https://dogs-find-production.up.railway.app/api/qestions');
-          const data = await response.json();
-          setQuestionData(data);
-        } catch (error) {
-          console.error('Error fetching question data:', error);
-        }
-      };
-  
-      fetchQuestionData();
-    }, []);
-  
-    const handleNextQuestion = () => {
-      if (currentQuestionIndex < questionData.length - 1) {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-      } else {
-        console.log('End of questions');
-      }
-    };
-  
-    if (!questionData) {
-      return <p>Loading...</p>;
+      ) : (
+        <p>No options available</p>
+      )}
+      <button onClick={checkAnswer}>Check Answer</button>
+      {selectedAnswer && <p>{checkAnswer()}</p>}
+      <button onClick={onNext}>Next</button>
+    </div>
+  );
+};
+
+export default function QUESTIONS() {
+  const [questionData, setQuestionData] = useState(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < questionData.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      // Handle end of questions
+      // You can show a summary or perform any other actions
+      console.log('End of questions');
     }
-  
-    const currentQuestion = questionData[currentQuestionIndex];
-  
-    return (
-      <div>
-        <QuestionComponent questionData={currentQuestion} onNext={handleNextQuestion} />
-      </div>
-    );
+  }
+  if (!questionData) {
+    return <p>Loading...</p>;
   }
 
+  const currentQuestion = questionData[currentQuestionIndex];
+
+  return (
+    <div>
+      <QuestionComponent questionData={currentQuestion} onNext={handleNextQuestion} />
+    </div>
+  );
+}
